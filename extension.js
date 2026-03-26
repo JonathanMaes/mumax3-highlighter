58e5b07de7bed18b7f52120c9ca592d1c8fa2f8a
Jonathan Maes <jonathan145b@hotmail.com>
2026-03-26 17:39:33 +0100

    Add autocomplete and hover providers

--- extension.js
+++ extension.js
@@ -1,4 +1,8 @@
 const vscode = require('vscode');
+const api = require('./data/mumax3_api.json');
+
+
+// const log = vscode.window.createOutputChannel("mumax³ highlighter debug channel"); // For debug purposes during development
 
 function activate(context) {
 	// The runMumax command is referenced in the package.json file
@@ -21,6 +25,79 @@ function activate(context) {
         terminal.show();
         terminal.sendText(`mumax3 "${filename}"`);
 	})); // Add to context subscriptions to ensure proper cleanup
+
+    // --- AUTOCOMPLETE PROVIDER FOR FUNCTIONS AND VARIABLES ---
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
+        { language: 'mumax3' },
+        {
+            provideCompletionItems(document, position, token, context) {
+                let items = [];
+
+                for (let [funcname, funcobj] of Object.entries(api.functions)) {
+                    const item = new vscode.CompletionItem(funcname, vscode.CompletionItemKind.Function);
+                    item.detail = funcobj.signature;
+                    item.documentation = new vscode.MarkdownString(funcobj.description);
+                    items.push(item);
+                }
+                
+                for (let [varname, varobj] of Object.entries(api.variables)) {
+                    const item = new vscode.CompletionItem(varname, vscode.CompletionItemKind.Variable);
+                    item.detail = varobj.signature;
+                    item.documentation = new vscode.MarkdownString(varobj.description);
+                    items.push(item);
+                }
+
+                return items;
+            }
+        }
+    ));
+
+    // -- AUTOCOMPLETE PROVIDER FOR METHODS (so only after typing "Identifier.") --
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
+        { language: 'mumax3' },
+        {
+            provideCompletionItems(document, position, token, context) {
+                // Detect pattern "Identifier(args)." before cursor
+                const line = document.lineAt(position.line).text;
+                const beforeCursor = line.substring(0, position.character);
+                const match = beforeCursor.match(/([A-Za-z_][A-Za-z0-9_]*)(?:\([^()]*\))?\.$/);
+                if (!match) {return [];} // Only do method completion if writing a method followed by dot
+                const objectName = match[1]; // always the identifier only
+
+                // Build completion items
+                let items = [];
+
+                for (let [methname, methobj] of Object.entries(api.methods)) {
+                    const methodReceivers = new Set(methobj.usedby.map(x => x.toLowerCase()));
+                    if (objectName && methodReceivers.has(objectName.toLowerCase())) {
+                        const item = new vscode.CompletionItem(methname, vscode.CompletionItemKind.Method);
+                        item.detail = methobj.signature;
+                        item.documentation = new vscode.MarkdownString(methobj.description);
+                        items.push(item);
+                    }
+                }
+                return items
+            }
+        },
+        '.'  // trigger character, since these are methods
+    ));
+
+
+    // --- HOVER PROVIDER ---
+    context.subscriptions.push(vscode.languages.registerHoverProvider(
+        { language: 'mumax3' },
+        {
+            provideHover(document, position, token) {
+                const word = document.getText(document.getWordRangeAtPosition(position));
+                for (const [name, obj] of [].concat(Object.entries(api.functions), Object.entries(api.variables), Object.entries(api.methods))) {
+                    if (word.toLowerCase() === name.toLowerCase()) {
+                        return new vscode.Hover(new vscode.MarkdownString(`## \`${obj.signature}\`\n\n` + obj.description));
+                    }
+                }
+                return undefined;
+            }
+        }
+    ));
 }
 
 function deactivate() {
